18d8297af6d196b3e35fb8237ebd8c40d78469c8
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-15 15:00:44 -0400

    slider shows in form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
 
+const getCalculatedBmi = (number) => (<span>{number.bmi}</span>)
+
+
+const BmiForm = () => (
+  <form>
+    <label>Height</label>
+    <input type='range' min='40' max='96' name='height' />
+    <label>Weight</label>
+    <input type='range' min='70' max='400' name='weight' />
+    <input type='submit' value='Calculate' />
+  </form>
+)
+
 const App = () => {
   return (
-    <div className='container'>
+    <div>
       <h1>BMI Calculator</h1>
+      {BmiForm()}
     </div>
   );
 }
